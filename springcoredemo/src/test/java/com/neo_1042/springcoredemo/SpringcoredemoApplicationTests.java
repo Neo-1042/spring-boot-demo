d1880386c0db7b50ba63812c7b278fbe7ed47c3b
Neo-1042 <rafaelh.71642@gmail.com>
2025-08-29 13:58:20 -0600

    Add new project 'Constructor Injection Demo'

---
+++ springcoredemo/src/test/java/com/neo_1042/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo_1042.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
