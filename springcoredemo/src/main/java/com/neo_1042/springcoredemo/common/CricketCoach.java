fad0123edc152a270c8900fad6f5749259a0d0da
Neo-1042 <rafaelh.71642@gmail.com>
2025-10-09 22:55:07 -0600

    Add custom init() and destroy() methods
    Go back to SINGLETON and one single dependency

--- springcoredemo/src/main/java/com/neo_1042/springcoredemo/common/CricketCoach.java
+++ springcoredemo/src/main/java/com/neo_1042/springcoredemo/common/CricketCoach.java
@@ -2,6 +2,10 @@ package com.neo_1042.springcoredemo.common;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.stereotype.Component;
+// Demo for custom init() and custom destroy()
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 
 import org.springframework.context.annotation.Scope;
 
@@ -10,16 +14,31 @@ import org.springframework.context.annotation.Scope;
 
 // Spring Beans have the default scope = SINGLETON
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CricketCoach implements Coach {
 
 	public CricketCoach() {
 		System.out.println("In constructor: " + getClass().getSimpleName());
 	}
 
+	// Define custom init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println("Initializing CricketCoach");
+		System.out.println("Firing engines ... ");
+		System.out.println(getClass().getSimpleName());
+	}
+
 	@Override
 	public String getDailyWorkout() {
-
 		return "DISCIPLINE AND TECHNIQUE WILL GRANT YOU TRUE HAPPINESS.";
+	}
+
+	// Define custom destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println("About to shut down");
+		System.out.println(" ... ");
+		System.out.println(getClass().getSimpleName());
 	}
 }
--- springcoredemo/src/main/java/com/neo_1042/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/neo_1042/springcoredemo/rest/DemoController.java
@@ -16,37 +16,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 * REMINDER: setter injection is used when you have optional dependencies
 */
 
+// Demo: Bean Scope Lifecycle: go back to SINGLETON scope
+// Inject only one dependency
 @RestController
 public class DemoController {
 
 	// Define a private field for the dependency to be injected
 	private Coach myCoach;
-	private Coach anotherCoach;
+	// private Coach anotherCoach;
 
 	// Update the constructor to INJECT another Coach
 	@Autowired
 	public void DemoController(
-			@Qualifier("cricketCoach") Coach theCoach,
-			@Qualifier("cricketCoach") Coach theAnotherCoach
+			@Qualifier("cricketCoach") Coach theCoach
+			//,@Qualifier("cricketCoach") Coach theAnotherCoach
 	) {
 		System.out.println("In constructor: " + getClass().getSimpleName());
 		myCoach = theCoach;
-		anotherCoach = theAnotherCoach;
+		//anotherCoach = theAnotherCoach;
 	}
 
 	@GetMapping("/dailyworkout")
 	public String getDailyWorkout() {
 
 		return myCoach.getDailyWorkout();
-	}
-
-	@GetMapping("/check-scope")
-	public String checkScope() {
-		StringBuilder s1 = new StringBuilder("Are the instanced beans the same?\n");
-			s1.append("theCoach == theAnotherCoach ----> \n");
-			s1.append(myCoach == anotherCoach);
-			System.out.println(s1.toString());
-		return s1.toString();
 	}
 
 }
