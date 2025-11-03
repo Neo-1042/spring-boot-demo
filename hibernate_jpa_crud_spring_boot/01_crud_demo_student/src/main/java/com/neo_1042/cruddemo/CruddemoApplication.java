bbddac0f81a0188ed0037642ed9eb8cbf29fc87e
Neo-1042 <rafaelh.71642@gmail.com>
2025-11-03 09:30:44 -0600

    Set up MySQL DB connection, runner lambda expression
    Successful connection to 'student_tracker' schema

---
+++ hibernate_jpa_crud_spring_boot/01_crud_demo_student/src/main/java/com/neo_1042/cruddemo/CruddemoApplication.java
@@ -0,0 +1,28 @@
+package com.neo_1042.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.beans.BeanProperty;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	// From the Spring Boot Framework
+	// It will get executed after the Spring Beans have been loaded.
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello, world, from " + getClass().getSimpleName());
+		};
+	}
+}
+
+
---
+++ hibernate_jpa_crud_spring_boot/01_crud_demo_student/src/test/java/com/neo_1042/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo_1042.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
