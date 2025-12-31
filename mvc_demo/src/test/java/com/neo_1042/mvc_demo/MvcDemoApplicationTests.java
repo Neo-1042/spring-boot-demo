fd9b1c3433a8f243233f0c84e37b7d0858549ccf
Neo-1042 <rafaelh.71642@gmail.com>
2025-12-30 19:48:27 -0600

    MVC DemoRestController intial commit
    @RestController, @RequestMapping, @GetMapping

---
+++ mvc_demo/src/test/java/com/neo_1042/mvc_demo/MvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo_1042.mvc_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
