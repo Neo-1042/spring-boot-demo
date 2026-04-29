cee6816e9bea3e5ad705676790065bb02e6ce67f
Neo-1042 <rafaelh.71642@gmail.com>
2026-04-28 22:11:48 -0600

    Re do limits_service with hard coded limit values
    No config server yet

--- microservices_projects/limits_service/src/main/java/com/neo_1042/microservices/limits_service/bean/Limits.java
+++ microservices_projects/limits_service/src/main/java/com/neo_1042/microservices/limits_service/bean/Limits.java
@@ -1,12 +1,11 @@
 package com.neo_1042.microservices.limits_service.bean;
 
 public class Limits {
-
     private int minimum;
 
     private int maximum;
 
-    public Limits() { }
+    public Limits() {}
 
     public Limits(int minimum, int maximum) {
         this.minimum = minimum;
@@ -16,18 +15,14 @@ public class Limits {
     public int getMinimum() {
         return minimum;
     }
-
     public void setMinimum(int minimum) {
         this.minimum = minimum;
     }
-
     public int getMaximum() {
         return maximum;
     }
-
     public void setMaximum(int maximum) {
         this.maximum = maximum;
     }
-
     
 }
--- microservices_projects/limits_service/src/main/java/com/neo_1042/microservices/limits_service/controller/LimitsController.java
+++ microservices_projects/limits_service/src/main/java/com/neo_1042/microservices/limits_service/controller/LimitsController.java
@@ -1,21 +1,15 @@
 package com.neo_1042.microservices.limits_service.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.neo_1042.microservices.limits_service.bean.Limits;
-import com.neo_1042.microservices.limits_service.configuration.Configuration;
 
 @RestController
 public class LimitsController {
 
-    // Make use of the Configuration class' properties:
-    @Autowired
-    private Configuration config;
-
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(config.getMinimum(), config.getMaximum());
+        return new Limits(1,1042);
     }
 }
