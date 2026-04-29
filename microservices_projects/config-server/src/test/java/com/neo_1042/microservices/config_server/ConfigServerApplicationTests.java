9bb0756d2211f6f30e6e25310c5ca728e332c5e0
Neo-1042 <rafaelh.71642@gmail.com>
2026-04-28 22:43:39 -0600

    Add properties configuration file for Spring Cloud Config Server

---
+++ microservices_projects/config-server/src/test/java/com/neo_1042/microservices/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo_1042.microservices.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
