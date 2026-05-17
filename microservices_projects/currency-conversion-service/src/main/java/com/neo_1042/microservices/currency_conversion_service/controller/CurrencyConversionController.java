095aaf883a841476a893978d35360a1b756f6865
Neo-1042 <rafaelh.71642@gmail.com>
2026-05-16 20:26:20 -0600

    Add OpenFeign Proxy
    @EnableFeignClients

--- microservices_projects/currency-conversion-service/src/main/java/com/neo_1042/microservices/currency_conversion_service/controller/CurrencyConversionController.java
+++ microservices_projects/currency-conversion-service/src/main/java/com/neo_1042/microservices/currency_conversion_service/controller/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.neo_1042.microservices.currency_conversion_service.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.neo_1042.microservices.currency_conversion_service.CurrencyConversion;
+import com.neo_1042.microservices.currency_conversion_service.CurrencyExchangeProxy;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
     
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -35,6 +40,21 @@ public class CurrencyConversionController {
         return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, 
             currencyConversion.getConversionMultiple(),
             quantity.multiply(currencyConversion.getConversionMultiple()),
-            currencyConversion.getEnvironment());
+            currencyConversion.getEnvironment() + " RestTemplate");
     }
+
+    // New method to make use of Open Feign instead of the RestTemplate:
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity) {
+        
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " FEIGN");
+    }
+
 }
--- spring_boot_notes/microservices/06_CURRENCY_PROJECT.md
+++ spring_boot_notes/microservices/06_CURRENCY_PROJECT.md
@@ -232,6 +232,54 @@ Response Structure:
 
 ### Use RestTemplate to make REST calls
 
+To make an API REST call, we had to code all of this in the
+controller:
+
+File = CurrencyConversionController.java
 ```java
 import org.springframework.web.client.RestTemplate;
+
+// ...
+@RestController
+public class CurrencyConversionController {
+    
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+        @PathVariable String from,
+        @PathVariable String to,
+        @PathVariable BigDecimal quantity
+    ) {
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        // REST call to the currency-exchange service
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+            "http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
+            CurrencyConversion.class, uriVariables);
+        
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, 
+            currencyConversion.getConversionMultiple(),
+            quantity.multiply(currencyConversion.getConversionMultiple()),
+            currencyConversion.getEnvironment());
+    }
+}
 ```
+
+**Spring Cloud Feign** makes this process simpler.
+
+# Spring Cloud Open Feign
+
+File = pom.xml
+```xml
+<dependencies>
+    <dependency>
+        <groupId>org.springframework.cloud</groupId>
+        <artifactId>spring-cloud-starter-openfeign</artifactId>
+    </dependency>
+</dependencies>
+```
+
